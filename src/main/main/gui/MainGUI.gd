ae00e5e6cd50f8123fc6323937b537efa887b1a3
raojm <raojm@91ne.com>
2023-05-11 10:47:11 +0800

    update

--- src/main/main/gui/MainGUI.gd
+++ src/main/main/gui/MainGUI.gd
@@ -7,7 +7,7 @@ var score := 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	TranslationServer.set_locale("en")
+	#TranslationServer.set_locale("zh")
 	update_ui()
 	$WindowIntro.popup_centered()
 	#$Window.child_controls_changed()
